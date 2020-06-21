ec29872811b365c82b0db953a35b8fb72dca2a54
Erwin Rudi <erwin.rudi@gmail.com>
2020-06-21 10:47:19 +0200

    more confinient structure

--- about/views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-about = Blueprint("about", __name__, static_folder="static", template_folder="templates")
-
-
-@about.route("/")
-def about_view():
-    return render_template("about.html")
--- blog/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-
-blog = Blueprint("blog", __name__, static_folder="static", template_folder="templates")
-
-
-@blog.route("/")
-def blog_view():
-    return render_template("blog.html")
