42e8b12cae286fd5f7744ebe56f31a4fb84891d7
Erwin Rudi <erwin.rudi@gmail.com>
2020-07-04 17:39:03 +0200

    index.html: blog from article post + about.html

--- portfolio_app/home.py
+++ portfolio_app/home.py
@@ -8,4 +8,15 @@ bp = Blueprint("home", __name__)
 
 @bp.route("/")
 def home_view():
-    return render_template("/home/index.html")
+    db = get_db()
+    posts = db.execute(
+        "SELECT p.id, title, intro, body, created, author_id, username"
+        " FROM post p JOIN user u ON p.author_id = u.id"
+        " ORDER BY created DESC"
+    ).fetchall()
+    return render_template("/home/index.html", posts=posts)
+
+
+@bp.route("/about")
+def about_view():
+    return render_template("/home/about.html")
