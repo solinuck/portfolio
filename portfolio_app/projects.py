9be31e9cbce5560e0785aa402c3c554ad8885d90
Erwin Rudi <erwin.rudi@gmail.com>
2020-08-11 14:48:58 +0200

    added projects-list page with all projects

--- portfolio_app/projects.py
+++ portfolio_app/projects.py
@@ -1,5 +1,5 @@
 from github import Github
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 
 
 bp = Blueprint("projects", __name__, url_prefix="/projects")
@@ -7,20 +7,31 @@ bp = Blueprint("projects", __name__, url_prefix="/projects")
 
 @bp.route("/")
 def projectlist_view():
-    return render_template("projects/projectlist.html")
+    username = "solinuck"
+
+    g = Github(current_app.config["GITHUB_TOKEN"])
+
+    user = g.get_user(username)
+    repos = user.get_repos()
+
+    project_img = []
+    i = 1
+    for repo in repos[:4]:
+        try:
+            project_img.append(
+                repo.get_contents("/img/readme-example.png").download_url
+            )
+        except:
+            project_img.append(
+                f"{current_app.config['IMAGES_PATH']}/projects/project-{i}.jpeg"
+            )
+            i += 1
+
+    return render_template(
+        "projects/projectlist.html", repos=repos, project_img=project_img
+    )
 
 
 @bp.route("/<name>")
 def single_project(name):
-    username = "solinuck"
-    url = "https://api.github.com/users/{}".format(username)
-
-    g = Github()
-
-    user = g.get_user(username)
-
-    for repo in user.get_repos():
-        readme = repo.get_readme().decoded_content
-    return render_template(
-        "projects/single_project.html", name=name, readme=readme, url=url
-    )
+    return render_template("projects/single_project.html")
