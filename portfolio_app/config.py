2594d1e6cf3ed3ec5e1e6639d3f00ed520410ba6
Erwin Rudi <erwin.rudi@gmail.com>
2020-08-20 09:24:26 +0200

    image links url_for() + styling prefixes

--- portfolio_app/config.py
+++ portfolio_app/config.py
@@ -1,34 +1,22 @@
-import os
 import json
 
-with open("/etc/config.json") as config_file:
-     config = json.load(config_file)
+with open("/etc/portfolio_config.json") as config_file:
+    config = json.load(config_file)
+
 
 class Config(object):
     DEBUG = False
     TESTING = False
-    SECRET_KEY = config.get("SECRET_KEY")
-    APP_PATH = os.path.dirname(os.path.realpath(__file__))
-    BLOG_UPLOADS = f"{APP_PATH}/portfolio_app/static/images/blog_uploads"
-    PROJECT_UPLOADS = f"{APP_PATH}/portfolio_app/static/images/projects"
-    IMAGES_PATH = "http://localhost:5000/static/images"
-    IMAGE_UPLOADS_REL = "http://localhost:5000/static/images/blog_uploads"
     GITHUB_TOKEN = config.get("GITHUB_TOKEN")
     SQLALCHEMY_DATABASE_URI = config.get("SQLALCHEMY_DATABASE_URI")
 
 
 class ProductionConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = config.get("SQLALCHEMY_DATABASE_URI")
+    SECRET_KEY = config.get("SECRET_KEY")
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    APP_PATH = os.path.dirname(os.path.realpath(__file__))
 
     SECRET_KEY = "dev"
-
-    BLOG_UPLOADS = f"{APP_PATH}/portfolio_app/static/images/blog_uploads"
-
-
-class TestingConfig(Config):
-    TESTING = True
